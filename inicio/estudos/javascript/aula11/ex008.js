ab4f151fa3a836173bfdf4c3dce8b1b7613bb629
mvmdchagas <43212259+mvmdchagas@users.noreply.github.com>
2022-08-09 17:29:44 -0400

    treino js

---
+++ inicio/estudos/javascript/aula11/ex008.js
@@ -0,0 +1,8 @@
+//console.log ('estou aqui, olá, tudo certo')
+var vel = 60.5
+console.log(`a velocidade do seu carro é: ${vel}km/h`)
+if (vel > 60){
+    console.log ('velocidade permitida ultrapassada !')
+    console.log('status: multa  registrada')
+}
+console.log (`use sempre cinto de segurança!`)
---
+++ inicio/estudos/javascript/aula11/ex009.js
@@ -0,0 +1,8 @@
+var pais = 'brasil'
+console.log (`vivendo em ${pais}`)
+if (pais != 'brasil'){
+    console.log ('você é estrangeiro')
+
+}else{
+    console.log ('você é brasileiro')
+}

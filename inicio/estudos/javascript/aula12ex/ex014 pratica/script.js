fd075bb9912be1290a9c9877382d0edaf7189611
mvmdchagas <43212259+mvmdchagas@users.noreply.github.com>
2022-11-26 10:20:55 -0400

    relembrar

--- inicio/estudos/javascript/aula12ex/ex014 pratica/script.js
+++ inicio/estudos/javascript/aula12ex/ex014 pratica/script.js
@@ -1,17 +1,25 @@
 function carregar(){
-    var hora = new Date().getHours()
-    var msg = document.querySelector ('div p#msg')
-    var img = document.querySelector('div img#imagem')
-    msg.innerHTML = `agora é exatamente: ${hora} horas`
-    if (hora >= 0 && hora <= 12){
-        img.src = 'imagens/fotomanha.png'
-        document.body.style.background ='#f5bbad'
-    } else if (hora >= 13 && hora <= 18){
-        img.src = 'imagens/fototarde.png'
-        document.body.style.background ='#e48c22'
-    }else {
+    let horaAtual = new Date().getHours()
+    let min = new Date().getMinutes()
+    let msg = document.querySelector ('main div p#msg')
+    let img = document.querySelector ('main div img#imagem')
+    msg.innerHTML = `horario local: ${horaAtual} : ${min}`
+    
+    if (horaAtual >= 00 && horaAtual <=5){
+        img.src = 'imagens/fotomadrugada.png'
+        document.body.style.background = '#2d6c89'
+    }else
+        if(horaAtual >=6  && horaAtual <= 11){
+            img.src = 'imagens/fotomanha.png'
+            document.body.style.background = '#fff79e'
+    }else
+        if(horaAtual >=12 && horaAtual <=18){
+            img.src = 'imagens/fototarde.png'
+            document.body.style.background = '#ffb787'
+        }
+    else{
         img.src = 'imagens/fotonoite.png'
-        document.body.style.background ='#231909'
+        document.body.style.background = '#4d5b67'
     }
 
 }
--- inicio/estudos/javascript/aula12ex/ex014/script.js
+++ inicio/estudos/javascript/aula12ex/ex014/script.js
@@ -7,11 +7,12 @@
     if(hora >= 0 && hora < 12){
         img.src ='imagens/fotomanha.png'
         document.body.style.background = '#ffeda1'
-    } else if (hora >= 12 && hora <= 18){
-        img.src = 'imagens/fototarde.png'
-        document.body.style.background = '#bb858a'
-    }else {
-        img.src = 'imagens/fotonoite.png'
-        document.body.style.background = '#fe8d00'
-    }
+    } else 
+        if (hora >= 12 && hora < 18){
+            img.src = 'imagens/fototarde.png'
+            document.body.style.background = '#bb858a'
+        }else {
+            img.src = 'imagens/fotonoite.png'
+            document.body.style.background = '#fe8d00'
+         }
 }

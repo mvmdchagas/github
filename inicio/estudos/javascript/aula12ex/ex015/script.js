3a64f7029a8474b58f721ef26e9a1a56378dc6da
mvmdchagas <43212259+mvmdchagas@users.noreply.github.com>
2022-11-30 01:12:09 -0400

    relembrando

--- inicio/estudos/javascript/aula12ex/ex015/script.js
+++ inicio/estudos/javascript/aula12ex/ex015/script.js
@@ -1,50 +1,49 @@
 function verificar(){
-   var data = new Date()
-   var ano = data.getFullYear()
-   var fano = document.querySelector ('input#txtano')
-   var res = document.querySelector('div#res')
-   if (fano.value.length == 0 || fano.value > ano){
-    res.innerHTML = `<p><strong>[SISTEMA]</strong>: insira um valor de 4 digitos que não seja acima do ano atual.</p>`
-   } else{
-    var fgen = document.getElementsByName ('radgen')
-    var idade = ano - Number(fano.value)
-    
-    var genero = ''
-    var img = document.createElement ('img')
-    img.setAttribute ('id', 'foto')
-    if(fgen[0].checked){
-        genero = 'Masculino'
-        if (idade >=0 && idade <10){
+    var ano = new Date().getFullYear()
+    var fano = document.getElementById('txtano')
+    var res = document.querySelector('div p#res')
+    if(fano.value.length == 0 || fano.value > ano){
+        res.innerHTML = ('<p><strong>[ERRO]</strong> Dados inválidos</p>')
+    }else{
+        var generoEscolhido = document.getElementsByName('escolhaGenero')
+        var idade = ano - Number(fano.value)
+        var genero = ''
+
+        var img = document.createElement('img')
+        img.setAttribute('id', 'foto')
+
+        if(generoEscolhido[0].checked){
+           genero = '<p>Masculino</p>'
+           if (idade >= 0 && idade < 10){
             //criança
             img.setAttribute ('src', 'img/criancamasc250.png')
-        }else if(idade <21){
+           } else if(idade < 21){
             //jovem
             img.setAttribute ('src', 'img/jovemmasc250.png')
-        }else if (idade <50){
+           }else if (idade < 50){
             //adulto
             img.setAttribute ('src', 'img/adultomasc250.png')
-        }else{
-            //idoso
+           } else{
+            // idoso
             img.setAttribute ('src', 'img/idosomasc250.png')
+           }
+        }else if (generoEscolhido[1].checked){
+            genero = '<p>Feminino</p>'
+            if (idade >= 0 && idade < 10){
+                //criança
+                img.setAttribute ('src', 'img/criancafem250.png')
+               } else if(idade < 21){
+                //jovem
+                img.setAttribute ('src', 'img/jovemfem250.png')
+               }else if (idade < 50){
+                //adulto
+                img.setAttribute ('src', 'img/adultafem250.png')
+               } else{
+                // idoso
+                img.setAttribute ('src', 'img/idosafem250.png')
+               }
         }
-
-    }else if (fgen[1].checked){
-        genero = 'Feminino'
-        if (idade >=0 && idade <10){
-            //criança
-            img.setAttribute ('src', 'img/criancafem250.png')
-        }else if(idade <21){
-            //jovem
-            img.setAttribute ('src', 'img/jovemfem250.png')
-        }else if (idade <50){
-            //adulto
-            img.setAttribute ('src', 'img/adultafem250.png')
-        }else{
-            //idoso
-            img.setAttribute ('src', 'img/idosafem250.png')
-        }
+        res.innerHTML = `<p>${genero} com ${idade} anos</p>`
+        res.appendChild(img)
     }
-    res.innerHTML = `[SISTEMA] - ${genero} com ${idade} anos`
-    res.appendChild(img)
-   }
 }

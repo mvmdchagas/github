199d8b2547a3e09e2398070e5eb13d50465fb7de
mvmdchagas <43212259+mvmdchagas@users.noreply.github.com>
2022-12-09 22:44:28 -0400

    lets code react hve

--- App/meu-app-react/src/index.js
+++ App/meu-app-react/src/index.js
@@ -4,14 +4,15 @@ import App from './components/App';
 import App2 from './components/App2';
 import App3 from './components/App3';
 import App4 from './components/App4';
+import App5 from './components/App5';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App4 title="titulo">
+    <App5 title="titulo">
       conteudo
-    </App4>
+    </App5>
   </React.StrictMode>
 );
 reportWebVitals();

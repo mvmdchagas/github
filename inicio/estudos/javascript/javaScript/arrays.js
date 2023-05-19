ef3853548fe22f7954adb5287c99ef57e032f3fd
mvmdchagas <43212259+mvmdchagas@users.noreply.github.com>
2023-05-19 00:14:36 -0400

    aninhar arrays

--- inicio/estudos/javascript/javaScript/arrays.js
+++ inicio/estudos/javascript/javaScript/arrays.js
@@ -1,3 +1,8 @@
-const i = ["marcos", "venicio", "123","tesoura"]
+//arrays
+/*const i = ["marcos", "venicio", "123","tesoura"]
 const j = ["pedra", 123, "soco inglês"]
-console.log(i,j)
+console.log(i,j)*/
+
+//aninhar arrays
+const b = [["gato", 5], ["cachorro", 33]];
+console.log (b)

5bd5d81cb55dcf9306118b0b8b9237e9ea0aa2ba
mvmdchagas <43212259+mvmdchagas@users.noreply.github.com>
2023-05-19 23:51:06 -0400

    push

--- inicio/estudos/javascript/javaScript/arrays.js
+++ inicio/estudos/javascript/javaScript/arrays.js
@@ -4,5 +4,31 @@ const j = ["pedra", 123, "soco inglês"]
 console.log(i,j)*/
 
 //aninhar arrays
-const b = [["gato", 5], ["cachorro", 33]];
-console.log (b)
+/*const b = [["gato", 5], ["cachorro", 33]];
+console.log (b)*/
+
+//acessar array de dados com índices
+/*const dados = ["marcos", 123, "macaco-louco"];
+console.log (dados[2]);
+const dados2 = dados[0];
+console.log (dados2);*/
+
+//Modificar dados de array com Índices
+/*const b = [50, 60, 70]
+b[0]= 45
+console.log (b)*/
+
+//acessar arrays multidimensionais com índices
+/*const variasArrays = [
+    [1,2,3],
+    [4,5,6],
+    [7,8,9],
+    [["nabucodonosor", 8, "scaloba-loba"], 13, 14]
+];
+const chaveAcesso = variasArrays[3][0][1]
+console.log(chaveAcesso)*/
+
+//método push
+const arr1 = [1,2,3];
+arr1.push("hoje", 12,"50",[1,2,60]);
+console.log(arr1)
